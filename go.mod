6ec31fdd49ce1f0a677ae2b68a4bb11f1df28528
Jesse Chen <jesse@usepylon.com>
2025-06-26 20:15:57 -0700

    go.mod: upgrade mvdan.cc/sh

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/pylon-labs/taskrunner
 
-go 1.21
+go 1.23
+
+toolchain go1.23.10
 
 require (
 	github.com/fatih/color v1.16.0
@@ -9,8 +11,8 @@ require (
 	github.com/samsarahq/go v0.0.0-20230622164605-46203ca3a6d8
 	github.com/stretchr/testify v1.8.4
 	go.uber.org/multierr v1.11.0
-	golang.org/x/sync v0.6.0
-	mvdan.cc/sh v2.5.1-0.20180917103936-65d8e2a4b156+incompatible
+	golang.org/x/sync v0.8.0
+	mvdan.cc/sh/v3 v3.11.0
 )
 
 require (
@@ -18,7 +20,6 @@ require (
 	github.com/gdamore/encoding v0.0.0-20151215212835-b23993cbb635 // indirect
 	github.com/gdamore/tcell v1.1.1-0.20181029231221-017915a4d77d // indirect
 	github.com/jtolds/gls v4.20.0+incompatible // indirect
-	github.com/kr/pretty v0.3.1 // indirect
 	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/lucasb-eyer/go-colorful v0.0.0-20181028223441-12d3b2882a08 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -26,8 +27,8 @@ require (
 	github.com/mattn/go-runewidth v0.0.4-0.20181117163326-c88d7e5f2e24 // indirect
 	github.com/mitchellh/go-wordwrap v1.0.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2 // indirect
-	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/term v0.29.0 // indirect
 	golang.org/x/text v0.3.1-0.20181030141323-6f44c5a2ea40 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- shell/shell.go
+++ shell/shell.go
@@ -4,61 +4,114 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"os"
 	"strings"
 
-	"mvdan.cc/sh/interp"
-	"mvdan.cc/sh/syntax"
+	"mvdan.cc/sh/v3/expand"
+	"mvdan.cc/sh/v3/interp"
+	"mvdan.cc/sh/v3/syntax"
 )
 
-type RunOption func(*interp.Runner)
+// runConfig collects options prior to constructing the sh Runner.
+type runConfig struct {
+	stdin  io.Reader
+	stdout io.Writer
+	stderr io.Writer
+	env    map[string]string
+	dir    string
+}
+
+// RunOption configures execution of a shell command.
+type RunOption func(*runConfig)
 
 type ShellRun func(ctx context.Context, command string, options ...RunOption) error
 
 // Stdout sets the output location for the stdout of a command.
 func Stdout(writer io.Writer) RunOption {
-	return func(r *interp.Runner) {
-		r.Stdout = writer
+	return func(c *runConfig) {
+		c.stdout = writer
 	}
 }
 
 // Stderr sets the output location for the stderr of a command.
 func Stderr(writer io.Writer) RunOption {
-	return func(r *interp.Runner) {
-		r.Stderr = writer
+	return func(c *runConfig) {
+		c.stderr = writer
 	}
 }
 
 // Stdin pipes in the contents provided by the reader to the command.
 func Stdin(reader io.Reader) RunOption {
-	return func(r *interp.Runner) {
-		r.Stdin = reader
+	return func(c *runConfig) {
+		c.stdin = reader
 	}
 }
 
 // Env sets the environment variables for the command.
 func Env(vars map[string]string) RunOption {
-	return func(r *interp.Runner) {
+	return func(c *runConfig) {
+		if c.env == nil {
+			c.env = make(map[string]string)
+		}
 		for k, v := range vars {
-			r.Env.Set(k, v)
+			c.env[k] = v
 		}
 	}
 }
 
-// Run executes a shell command.
+// Dir sets the working directory for the command.
+func Dir(path string) RunOption {
+	return func(c *runConfig) {
+		c.dir = path
+	}
+}
+
+// Run executes a shell command using mvdan.cc/sh's interpreter.
 func Run(ctx context.Context, command string, opts ...RunOption) error {
+	// Parse the program
 	p, err := syntax.NewParser().Parse(strings.NewReader(command), "")
 	if err != nil {
 		return err
 	}
 
-	r, err := interp.New()
-	if err != nil {
-		panic(fmt.Errorf("failed to set up interpreter: %s", err.Error()))
-	}
-
+	// Build configuration from provided options
+	cfg := &runConfig{}
 	for _, opt := range opts {
-		opt(r)
+		opt(cfg)
 	}
 
+	// Default std streams
+	if cfg.stdin == nil {
+		cfg.stdin = os.Stdin
+	}
+	if cfg.stdout == nil {
+		cfg.stdout = os.Stdout
+	}
+	if cfg.stderr == nil {
+		cfg.stderr = os.Stderr
+	}
+
+	runnerOpts := []interp.RunnerOption{
+		interp.StdIO(cfg.stdin, cfg.stdout, cfg.stderr),
+	}
+
+	// Build environment pairs
+	envPairs := os.Environ()
+	for k, v := range cfg.env {
+		envPairs = append(envPairs, fmt.Sprintf("%s=%s", k, v))
+	}
+	runnerOpts = append(runnerOpts, interp.Env(expand.ListEnviron(envPairs...)))
+
+	if cfg.dir != "" {
+		runnerOpts = append(runnerOpts, interp.Dir(cfg.dir))
+	}
+
+	// Construct the runner
+	r, err := interp.New(runnerOpts...)
+	if err != nil {
+		return fmt.Errorf("failed to set up interpreter: %w", err)
+	}
+
+	// Execute the parsed program
 	return r.Run(ctx, p)
 }
